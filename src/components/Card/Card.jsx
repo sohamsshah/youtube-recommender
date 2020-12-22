0cb68b0dfcfd68af9fd58387607930eb9d103ca9
sohamsshah <sohamshah456@gmail.com>
2020-12-22 12:36:36 +0530

    solve:images, data

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,7 +3,7 @@ export default function Card(props){
     return(
         <div class="outer-card" style={{padding:"2rem"}}>
         <div class="card" style={{width: "18rem", padding:"2rem"}}>
-      <img class="card-img-top" src="..." alt="Card image cap"></img>
+      <img class="card-img-top" src={props.channel.img} alt="Card image cap"></img>
         <div class="card-body">
           <h5 class="card-title"><h4><b>{props.channel.name}</b></h4> </h5>
           <p class="card-text"><ul className="tags"> {`Tags:\n`} 
@@ -11,7 +11,7 @@ export default function Card(props){
         <li className="list-non-bullet tag list-item-inline">{tag+" "}</li>
         ))}
          </ul></p>
-          <a href={props.channel.url} class="btn">Go somewhere</a>
+          <a href={props.channel.url} class="btn">Visit Channel</a>
         </div>
         </div>
   </div>
--- src/data.js
+++ src/data.js
@@ -1,38 +1,70 @@
+import adityaVerma from './images/adityaVerma.jpg'
+import tanayPratap from './images/tanayPratap.jpg'
+import anshika from './images/anshika.jpg'
+import benAwad from './images/benAwad.jpg'
+import edureka from './images/edureka.png'
+import nickWhite from './images/nickWhite.jpg'
+import sentdex from './images/sentdex.jpg'
+import simplilearn from './images/simplilearn.jpg'
+import thenetninja from './images/thenetninja.png'
+import traversyMedia from './images/traversyMedia.jpg'
+import webDevSimplified from './images/webDevSimplified.jpg'
+import techDose from './images/techDose.jpg'
+import takeUForward from './images/takeUForward.jpg'
+
+
+
+
 const data = {
     "Web Development": [
       { name: "Tanay Pratap", url: "https://www.youtube.com/channel/UCNFmBuclxQPe57orKiQbyfA",
-      tags: ["LIVE classes", "JavaScript"] },
+      tags: ["LIVE classes", "JavaScript"],
+      img: tanayPratap },
       { name: "Ben Awad", url: "https://www.youtube.com/channel/UCNFmBuclxQPe57orKiQbyfA",
-      tags: ["React", "projects"] },
+      tags: ["React", "projects"],
+    img: benAwad},
       { name: "The Net Ninja", url: "https://www.youtube.com/channel/UCNFmBuclxQPe57orKiQbyfA",
-      tags: ["Firebase", "GraphQL"] },
+      tags: ["Firebase", "GraphQL"],
+    img: thenetninja },
       { name: "Traversy Media", url: "https://www.youtube.com/channel/UCNFmBuclxQPe57orKiQbyfA",
-      tags: ["tutorials", "JavaScript"] },
+      tags: ["tutorials", "JavaScript"],
+    img: traversyMedia},
       { name: "Web Dev Simplified", url: "https://www.youtube.com/channel/UCNFmBuclxQPe57orKiQbyfA",
-      tags: ["projects", "tutorials"] },
+      tags: ["projects", "tutorials"],
+    img: webDevSimplified },
     ],
   
     "Machine Learning": [
         { name: "Sentdex", url: "https://www.youtube.com/channel/UCNFmBuclxQPe57orKiQbyfA",
-        tags: ["python", "neural networks"] },
+        tags: ["python", "neural networks"],
+      img: sentdex },
         { name: "Edureka", url: "https://www.youtube.com/channel/UCNFmBuclxQPe57orKiQbyfA",
-        tags: ["interview", "projects"] },
+        tags: ["interview", "projects"],
+      img: edureka},
         { name: "simplilearn", url: "https://www.youtube.com/channel/UCNFmBuclxQPe57orKiQbyfA",
-        tags: ["interview", "mathematics"] },
+        tags: ["interview", "mathematics"],
+      img: simplilearn },
     ],
     "Competitve Coding": [
         { name: "Aditya Verma", url: "https://www.youtube.com/channel/UCNFmBuclxQPe57orKiQbyfA",
-        tags: ["DP", "Recursion"] },
+        tags: ["DP", "Recursion"], 
+      img: adityaVerma},
         { name: "Anshika Gupta", url: "https://www.youtube.com/channel/UCNFmBuclxQPe57orKiQbyfA",
-        tags: ["guidance", "coding"] },
-        { name: "Striver_79", url: "https://www.youtube.com/channel/UCNFmBuclxQPe57orKiQbyfA",
-        tags: ["tutorials", "guidance"] },
+        tags: ["guidance", "coding"],
+      img: anshika},
+        { name: "takeUForward", url: "https://www.youtube.com/channel/UCNFmBuclxQPe57orKiQbyfA",
+        tags: ["tutorials", "guidance"],
+      img: takeUForward },
         { name: "Nick White", url: "https://www.youtube.com/channel/UCNFmBuclxQPe57orKiQbyfA",
-        tags: ["Leetcode", "Java"] },
+        tags: ["Leetcode", "Java"],
+      img: nickWhite },
         { name: "Tech Dose", url: "https://www.youtube.com/channel/UCNFmBuclxQPe57orKiQbyfA",
-        tags: ["Leetcode", "interview"] },
+        tags: ["Leetcode", "interview"],
+      img: techDose },
         
     ]
   };
+
+
   
   export default data;
